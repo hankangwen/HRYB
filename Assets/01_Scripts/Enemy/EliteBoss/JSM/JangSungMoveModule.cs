abbe272fadf6349f3cdbb23820425b986c80460d
yuchul06 <65890837+yuchul06@users.noreply.github.com>
2024-04-01 19:30:51 +0900

    AVCD

--- Assets/01_Scripts/Enemy/EliteBoss/JSM/JangSungMoveModule.cs
+++ Assets/01_Scripts/Enemy/EliteBoss/JSM/JangSungMoveModule.cs
@@ -9,15 +9,15 @@ public class JangSungMoveModule : MoveModule
 	Actor _target;
 	UnityEngine.AI.NavMeshAgent agent;
 
-	[Header("FIND")] [SerializeField] private float _findPlayerRange = 30;
-	
-	[Header("Stat")] 
+	[Header("FIND")][SerializeField] private float _findPlayerRange = 30;
+
+	[Header("Stat")]
 	[SerializeField] private float _normalSpeed = 3.5f;
 	[SerializeField] private float _fallDownMoveSpeed = 9f;
 
 	private bool _isMove = false;
 	private bool _moveDecalOnShot = false;
-	public override float Speed { get => base.Speed; set{ base.Speed = value; agent.speed = base.Speed; } }
+	public override float Speed { get => base.Speed; set { base.Speed = value; agent.speed = base.Speed; } }
 
 	private void Awake()
 	{
@@ -28,7 +28,7 @@ public class JangSungMoveModule : MoveModule
 	public override void Move()
 	{
 
-		
+
 	}
 
 	public void SetTarget(Actor _target)
@@ -49,20 +49,24 @@ public class JangSungMoveModule : MoveModule
 
 	public override void FixedUpdate()
 	{
-		
+
 	}
 	private void Update()
 	{
 		if (_isMove)
 		{
 
-			if(_target != null)
+			if (_target != null)
 			{
-				UnityEngine.AI.NavMesh.SamplePosition( _target.transform.position, out UnityEngine.AI.NavMeshHit hit, 15f, UnityEngine.AI.NavMesh.AllAreas);
-				agent.speed = _fallDownMoveSpeed;
-				agent.SetDestination(hit.position);
+				UnityEngine.AI.NavMesh.SamplePosition(_target.transform.position, out UnityEngine.AI.NavMeshHit hit, 15f, UnityEngine.AI.NavMesh.AllAreas);
+				if (agent.enabled)
+				{
+					agent.speed = _fallDownMoveSpeed;
+					agent.SetDestination(hit.position);
+				}
+
 				//Debug.LogError(hit.position);
-//			Debug.LogWarning(_target.transform.position);
+				//			Debug.LogWarning(_target.transform.position);
 				//GetActor().anim.SetMoveState();
 			}
 		}
@@ -74,7 +78,7 @@ public class JangSungMoveModule : MoveModule
 	{
 		_isMove = true;
 		_moveDecalOnShot = true;
-		if(_moveDecalOnShot)
+		if (_moveDecalOnShot)
 		{
 			_moveDecalOnShot = false;
 			GameObject obj = PoolManager.GetObject("MiddleBoxDecal", transform);
@@ -82,55 +86,61 @@ public class JangSungMoveModule : MoveModule
 
 			if (obj.TryGetComponent<BoxDecal>(out BoxDecal box))
 			{
-				box.SetUpDecal(transform, new Vector3(0,0,0), new Vector3(1,1,1));
+				box.SetUpDecal(transform, new Vector3(0, 0, 0), new Vector3(1, 1, 1));
 				box.StartDecal(2f);
 			}
 		}
-		
-//		Debug.LogWarning("AAAAAAZAAAAAA");
+
+		//		Debug.LogWarning("AAAAAAZAAAAAA");
 
 		//Debug.LogError("실행됨11");
 	}
-	
+
 
 	public void NormalMoveAttack()
 	{
-		if(_target != null)
+		if (_target != null)
 		{
 			UnityEngine.AI.NavMeshHit hit;
-			
+
 			Vector3 vec = (_target.transform.position - transform.position);
 			vec.y = 0;
-			
-			
+
+
 			vec = vec.normalized * 8 + transform.position;
 
 			if (Vector3.Distance(vec, _target.transform.position) < 5)
 			{
-				
-				UnityEngine.AI.NavMesh.SamplePosition(_target.transform.position, out  hit, 8f, UnityEngine.AI.NavMesh.AllAreas);
+
+				UnityEngine.AI.NavMesh.SamplePosition(_target.transform.position, out hit, 8f, UnityEngine.AI.NavMesh.AllAreas);
+				agent.speed = _normalSpeed;
+				agent.SetDestination(hit.position);
 			}
 			else
 			{
 				UnityEngine.AI.NavMesh.SamplePosition(vec, out hit, 8, UnityEngine.AI.NavMesh.AllAreas);
-				
+				agent.speed = _normalSpeed;
+				agent.SetDestination(hit.position);
+
 			}
-			
+
 			GameObject obj = PoolManager.GetObject("MiddleBoxDecal", transform);
 
 			if (obj.TryGetComponent<BoxDecal>(out BoxDecal box))
 			{
-				box.SetUpDecal(new Vector3(0, 0, 7), transform.rotation, new Vector3(0.55f,0.55f,0.55f), Vector3.zero, Vector3.one );
+				box.SetUpDecal(new Vector3(0, 0, 7), transform.rotation, new Vector3(0.55f, 0.55f, 0.55f), Vector3.zero, Vector3.one);
 				box.StartDecal(1f);
 			}
 			else
 			{
 				Debug.LogError("프리팹 없음?");
 			}
-			
+
 			//Debug.LogError("실행됨22");
-			agent.speed = _normalSpeed;
-			agent.SetDestination(hit.position);
+
+
+
+
 			//GetActor().anim.SetMoveState();
 		}
 	}
@@ -138,9 +148,11 @@ public class JangSungMoveModule : MoveModule
 	public void ResetDest()
 	{
 		_isMove = false;
+
 		agent.speed = 0;
 		agent.velocity = new Vector3(0, 0, 0);
 		UnityEngine.AI.NavMesh.SamplePosition(transform.position, out UnityEngine.AI.NavMeshHit hit, 15f, UnityEngine.AI.NavMesh.AllAreas);
 		agent.SetDestination(hit.position);
+
 	}
 }
