480533c9cc1ab02c70f7daff9c198cc726e5fea0
YouJunHyun <tsumego2015@gmail.com>
2024-04-01 19:30:39 +0900

    barrier added

--- Assets/01_Scripts/BreakableLifeModule.cs
+++ Assets/01_Scripts/BreakableLifeModule.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class BreakableLifeModule : LifeModule
 {
 	public string destroyedEff;
+	public float fadeSec;
+
+	Material mat;
+
+	public override void Awake()
+	{
+		base.Awake();
+		mat = GetComponent<Renderer>().material;
+	}
+
 	public override void OnDead()
 	{
-		gameObject.SetActive(false);
+		StartCoroutine(DelFader());
 		PoolManager.GetObject(destroyedEff, transform.position, Quaternion.identity, 5f);
+	}
+
+	IEnumerator DelFader()
+	{
+		float t = 0;
+		while(t <= fadeSec)
+		{
+			t += Time.deltaTime;
+			mat.SetFloat("_Alpha", 1 - (t / fadeSec));
+			yield return null;
+		}
+		gameObject.SetActive(false);
 	}
 }
