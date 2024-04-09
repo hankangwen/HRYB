c71009c44f296f4293f6d386ba28b25ae246136f
YouJunHyun <tsumego2015@gmail.com>
2024-04-09 13:00:15 +0900

    quest final refine with node creation complete

---
+++ Assets/01_Scripts/Managers/Nodes/NodeUtility.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum StatUpgradeType
+{
+	White,
+	Black,
+	Atk,
+	MoveSpeed,
+	CooldownRdc,
+}
+
+public class NodeUtility
+{
+
+	public const string NODEPATH = "Nodes/";
+
+	public const string NODEFILENAME = "Node";
+
+	public static List<List<PlayerNode>> LoadNodeData()
+	{
+		List<List<PlayerNode>> res = new List<List<PlayerNode>>();
+		List<PlayerNode> datas = Resources.LoadAll<PlayerNode>(NODEPATH).ToList();
+
+		string[] splitter;
+		for (int i = 0; i < datas.Count; i++) //무조건 순섲대로 정렬되어있다는 가정하에 한다.
+		{
+			splitter = datas[i].name.Split('_');
+			int circleIdx = int.Parse(splitter[0]);
+			if(res.Count <= circleIdx)
+			{
+				res.Add(new List<PlayerNode>());
+			}
+			res[circleIdx].Add(datas[i]);
+		}
+
+		return res;
+	}
+
+	public static List<PlayerNode> LoadNodeData(int circleIdx)
+	{
+		List<PlayerNode> res = new List<PlayerNode>();
+		List<PlayerNode> datas = Resources.LoadAll<PlayerNode>(NODEPATH).ToList();
+
+		string[] splitter;
+		for (int i = 0; i < datas.Count; i++) //무조건 순섲대로 정렬되어있다는 가정하에 한다.
+		{
+			splitter = datas[i].name.Split('_');
+			int cIdx = int.Parse(splitter[0]);
+			if(cIdx == circleIdx)
+			{
+				res.Add(datas[i]);
+			}
+		}
+
+		return res;
+	}
+
+	public static PlayerNode LoadNodeData(int circleIdx, int orderIdx)
+	{
+		PlayerNode res = null;
+		List<PlayerNode> datas = Resources.LoadAll<PlayerNode>(NODEPATH).ToList();
+
+		string[] splitter;
+		for (int i = 0; i < datas.Count; i++) //무조건 순섲대로 정렬되어있다는 가정하에 한다.
+		{
+			splitter = datas[i].name.Split('_');
+			int cIdx = int.Parse(splitter[0]);
+			int oIdx = int.Parse(splitter[1]);
+			if (cIdx == circleIdx && oIdx == orderIdx)
+			{
+				res = datas[i];
+				break;
+			}
+		}
+
+
+		return res;
+	}
+
+	public static string ToStringKorean(StatUpgradeType act)
+	{
+		switch (act)
+		{
+			case StatUpgradeType.White:
+				return "양(하얀거)";
+			case StatUpgradeType.Black:
+				return "음(검은거)";
+			case StatUpgradeType.Atk:
+				return "공격";
+			case StatUpgradeType.MoveSpeed:
+				return "이동속도";
+			case StatUpgradeType.CooldownRdc:
+				return "쿨다운 감소";
+			default:
+				Debug.LogWarning($"{act} 상태에 대한 한글 번역이 제공되지 않습니다.");
+				return act.ToString();
+		}
+	}
+}
