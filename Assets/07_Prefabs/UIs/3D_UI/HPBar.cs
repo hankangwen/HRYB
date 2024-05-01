f6172750afe7a22b61a5ebccf99d514a84cbd6d1
yuseungwon0318 <sw060318@naver.com>
2024-05-01 19:30:02 +0900

    rr

--- Assets/07_Prefabs/UIs/3D_UI/HPBar.cs
+++ Assets/07_Prefabs/UIs/3D_UI/HPBar.cs
@@ -11,18 +11,34 @@ public class HPBar : MonoBehaviour
 	public Image hpBarBlack;
 	public Image hpBarWhite;
 
+	float value = 0;
+
 	private void Awake()
 	{
 
 		hpBarBlack = transform.Find("BlackHP/Black").GetComponent<Image>();
 		hpBarWhite = transform.Find("WhiteHP/White").GetComponent<Image>();
-		lf = GetComponentInParent<LifeModule>(); 
+		
+		
+		lf = GetComponentInParent<LifeModule>();
+
+		if(lf.initYinYang.white > lf.initYinYang.black)
+		{
+			hpBarBlack.rectTransform.sizeDelta = new Vector2();
+			value = lf.initYinYang.black / lf.initYinYang.white;
+		}
+		else
+		{
+
+		}
 	}
 
 	private void Update()
 	{
 		hpBarWhite.fillAmount = lf.yy.white / lf.initYinYang.white;
 		hpBarBlack.fillAmount = lf.yy.black / lf.initYinYang.black;
+
+		
 
 		if ((lf.yy.white >= lf.initYinYang.white && lf.yy.black >= lf.initYinYang.black) || (lf.yy.white <= 0 && lf.yy.black <= 0))
 		{
